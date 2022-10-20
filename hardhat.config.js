0f09783c707f96e3b7d50e80870a872754e9d903
cryptodogge <crypto_dog@126.com>
2022-10-20 18:13:05 +0800

    update config

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,19 @@
 require("@nomiclabs/hardhat-waffle");
 
 module.exports = {
-  solidity: "0.8.10",
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.10',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
   networks: {
     rinkeby: {
       url: `https://rinkeby.infura.io/v3/7d00bf84530c4264969a4f0f231de8b6`,
